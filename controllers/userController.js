01ae54e7cbc98ff1eee8275299428c6c8fdc3b21
Suzi Nogueira <asuzinogueira@gmail.com>
2021-11-01 23:37:05 -0300

    created validation with join

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,9 +1,14 @@
 const User = require('../models/User')
 const bcrypt = require('bcryptjs')
 const jwt = require('jsonwebtoken')
+const { loginValidate, registerValidate } = require('./validate')
 
 const userController = {
   register: async function (req, res) {
+    //validando se o usuario informou o mínimo de caracteres solicitado
+    const { error } = registerValidate(req.body)
+    if (error) return res.status(400).send(error.message)
+
     // verificação para email já existente
     const selectedUser = await User.findOne({ email: req.body.email })
     if (selectedUser) return res.status(400).send('Email already exists')
@@ -24,6 +29,10 @@ const userController = {
     res.send('Register')
   },
   login: async function (req, res) {
+    //validando se o usuario informou o mínimo de caracteres solicitado
+    const { error } = loginValidate(req.body)
+    if (error) return res.status(400).send(error.message)
+
     const selectedUser = await User.findOne({ email: req.body.email })
     if (!selectedUser)
       return res.status(400).send('Email or password incorrect!')
